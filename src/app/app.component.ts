909a89a711537e0391e18e16d6b6544b683e9724
Waris Ali <Waris.Ali@philips.com>
2022-04-06 19:13:00 +0530

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import * as d3 from 'd3';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  gaugemap = {};
+  gaugemap:any;
   constructor() {}
   ngOnInit() {
     this.draw();
